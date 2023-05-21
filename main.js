dd5b37aa061e191b639d9b6b039ddb7004ec13bb
mouasit <mustaphaouassit60@gmail.com>
2023-05-21 02:26:50 +0100

    change link

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("hello from main.js");
+const svgButton = document.getElementById("download").querySelectorAll("a")[8];
+const editButton = document.getElementById("detail_edit_icon");
+const svgContent = document.getElementsByClassName("icon-holder");
+
+console.log(svgButton);
+
+svgButton.addEventListener("click", () => {
+  editButton.click();
+  const interval = setInterval(() => {
+    if (svgContent[0]?.querySelector("svg")) {
+      navigator.clipboard.writeText(
+        svgContent[0].querySelector("svg").outerHTML
+      );
+      clearInterval(interval);
+    }
+  }, 100);
+});
